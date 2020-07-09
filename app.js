55733d5f4998cc5e22b4a98f85c54e9f7a1264e2
naetharu <naetharu@gmail.com>
2020-07-09 16:44:09 +0100

    add character selection page

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.use(express.static("public"));
 const bio1 =
   "Morick is a dwarven ranger. He was once part of the Kur-Kurek Wilders, but that was a lifetime ago. These days he spends his time living a simple life. Based out of the Boatsman Inn in Ranshore, he makes a living as a guide and caravan guard for the many merchants moving their goods from Kur-Kurek into the New Kingdoms. The work is dangerous but it pays well enough. Between caravan jobs, Morick spends time out in the wilds. He has build a small cabin a few miles into the Wittlewood and spends as much time out there as he can. He finds the quiet and solitude suits him well.";
 
+const bio2 = "Hogan is human warrior. He was born and raised in Rottfurt, growing up in the waterfront district. He earned his stripes running with Herman Furg’s “Waterboy” gang. After the bleak winter of 1217 Rottfurt was abandoned and Hogan travelled south with the other refugees. He soon found himself in Ayac City where he made a living as a street boxer. After some time he was picked up by some of the promoters and made his way into the cities arena. Three years of fights and a mighty winning streak set Hogan up for life. Today he lives in a sizable townhouse where enjoys the finer things in life.";
+
+
 const characters = [
   {
     name: "Morick",
@@ -28,15 +31,15 @@ const characters = [
     },
     skills: {
       skill1: "Tracking",
-      skill2: "Wilderness Survival",
+      skill2: "Wilderness survival",
       skill3: "Trading",
       skill4: "Repair",
     },
     feats: {
-      feat1: "Danger Sense",
+      feat1: "Danger sense",
       feat2: "Clear vision",
       feat3: "Snap Shot",
-      feat4: "Stealthy Movement",
+      feat4: "Stealthy movement",
     },
     powers: {
       power1: "Patient shot",
@@ -44,11 +47,52 @@ const characters = [
       power3: "Throw knife",
       power4: "Haymaker",
     },
+    secStat: {
+      armour: 16,
+      reflex: 4,
+      willpower: 2,
+      toughness: 2,
+      melee: 3,
+      range: 5,
+    },
   },
   {
-    pic: "fem1",
-    name: "Charlie",
-    bio: bio1,
+    name: "Hogan",
+    bio: bio2,
+    stats: {
+      str: 16,
+      dex: 14,
+      con: 14,
+      int: 10,
+      wis: 10,
+      cha: 10,
+    },
+    skills: {
+      skill1: "Tactics",
+      skill2: "Street Smarts",
+      skill3: "Intimidation",
+      skill4: "Business",
+    },
+    feats: {
+      feat1: "Reposte",
+      feat2: "Second Wind",
+      feat3: "Quick Refelxes",
+      feat4: "Crowd Pleaser",
+    },
+    powers: {
+      power1: "Deadly Strike",
+      power2: "Concussion Blow",
+      power3: "Grappler",
+      power4: "Fancy Footwork",
+    },
+    secStat: {
+      armour: 20,
+      reflex: 4,
+      willpower: 2,
+      toughness: 4,
+      melee: 6,
+      range: 2,
+    },
   },
 ];
 
@@ -75,8 +119,15 @@ app.get("/character/:character", (req, res) => {
         skills: character.skills,
         feats: character.feats,
         powers: character.powers,
+        sec: character.secStat,
       });
     }
+  });
+});
+
+app.get("/characters", (req, res) => {
+  res.render("characters", {
+    characters: characters,
   });
 });
 
