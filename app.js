7e6a678b8294ef03249ce32ade593247fb3c43eb
naetharu <naetharu@gmail.com>
2020-07-09 10:10:04 +0100

    compelte character page v1

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require("lodash");
+const { forEach } = require("lodash");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+/* Data constants to create sample text --- to be removed! */
+
+const bio1 =
+  "Morick is a dwarven ranger. He was once part of the Kur-Kurek Wilders, but that was a lifetime ago. These days he spends his time living a simple life. Based out of the Boatsman Inn in Ranshore, he makes a living as a guide and caravan guard for the many merchants moving their goods from Kur-Kurek into the New Kingdoms. The work is dangerous but it pays well enough. Between caravan jobs, Morick spends time out in the wilds. He has build a small cabin a few miles into the Wittlewood and spends as much time out there as he can. He finds the quiet and solitude suits him well.";
+
+const characters = [
+  {
+    name: "Morick",
+    bio: bio1,
+    stats: {
+      str: 12,
+      dex: 14,
+      con: 12,
+      int: 10,
+      wis: 12,
+      cha: 08,
+    },
+    skills: {
+      skill1: "Tracking",
+      skill2: "Wilderness Survival",
+      skill3: "Trading",
+      skill4: "Repair",
+    },
+    feats: {
+      feat1: "Danger Sense",
+      feat2: "Clear vision",
+      feat3: "Snap Shot",
+      feat4: "Stealthy Movement",
+    },
+    powers: {
+      power1: "Patient shot",
+      power2: "Bolas",
+      power3: "Throw knife",
+      power4: "Haymaker",
+    },
+  },
+  {
+    pic: "fem1",
+    name: "Charlie",
+    bio: bio1,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/place", (req, res) => {
+  res.render("place");
+});
+
+app.get("/character/:character", (req, res) => {
+  const charReq = lodash.lowerCase(req.params.character);
+
+  characters.forEach((character) => {
+    const charCheck = lodash.lowerCase(character.name);
+
+    if (charReq === charCheck) {
+      res.render("character", {
+        pic: lodash.toLower(character.name),
+        name: character.name,
+        bio: character.bio,
+        stats: character.stats,
+        skills: character.skills,
+        feats: character.feats,
+        powers: character.powers,
+      });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000...");
+});
